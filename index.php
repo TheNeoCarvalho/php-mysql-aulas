30b79916c28325f6097e7e20ec636085334099b7
Manoel C <manoelmecat@gmail.com>
2018-08-14 09:42:14 -0300

    Reupando menu
    Reupando menu

--- index.php
+++ index.php
@@ -19,26 +19,29 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Dashboard Admin</title>
-	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script src="js/bootstrap.min.js"></script>
 	<style type="text/css">
-		.nav-link { color:#fff; font-weight: 700 }
+		.nav-link { width: 120px; color:#fff; font-weight: 700 }
 		.nav-link:hover { color: #000; font-weight: 700}
 		nav h3 { color: #fff }
 	</style>
 </head>
 <body>
 <nav class="navbar navbar-dark bg-primary">
- <h3>Seja bem vindo, <?php echo $user;?></h3>
+ <h3>Seja bem vindos, <?php echo $user;?></h3>
  <nav>
  <ul class="nav">
 	<?php
 		$sql = "SELECT * FROM menu";
 		$query = mysqli_query($conexao, $sql);
+		$m = array();
 		while($menus = mysqli_fetch_assoc($query)){
 			echo "<li class='nav-item'>
-		             <a class='nav-link' href='?page=".$menus['url']."'>".strtoupper($menus['titulo'])."</a>
+		             <a class='nav-link' href='?page=".$menus['url']."'><i class='".$menus['icon']."'></i> ".strtoupper($menus['titulo'])."</a>
 		  	      </li>";
+		  	array_push($m, $menus['titulo']);
 		}
 	?>
 </ul>
@@ -48,24 +51,14 @@
 	<div class="container">
 		<?php
 			if(isset($_REQUEST['page'])){
-				if($_REQUEST['page'] == 'home'){
-					include('pages/home.php');
-				}else if($_REQUEST['page'] == 'sobre'){
-					include('pages/sobre.php');
-				}else if($_REQUEST['page'] == 'artigos'){
-					include('pages/artigos.php');
-				}else if($_REQUEST['page'] == 'menu'){
-					include('pages/menu.php');
-				}else if($_REQUEST['page'] == 'portifolio'){
-					include('pages/portifolio.php');
-				}else if($_REQUEST['page'] == 'neo'){
-					include('pages/neo.php');
-				}else{
-					include('pages/go.php');
-				}
+				foreach ($m as $ms) {
+					if($_REQUEST['page'] == $ms){
+						include('pages/'.$ms.'.php');
+					}
+				}	
 			}else{
-				include('pages/go.php');
-			}
+				include('pages/home.php');
+		    }
 		?>
 	</div>
 </body>
